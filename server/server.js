11274dd8da5a4ab7c0434f793ec1929886dba749
Ruthvik Arya <ruthvikarya@outlook.com>
2017-04-11 17:54:40 -0700

    adding patch routes to edit todo items

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const _ = require('lodash');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -67,6 +68,33 @@ app.delete('/todos/:id', (req, res) => {
     }, (e) => {
         return res.status(400).send();
     })
+});
+
+//update routes
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _._.pick(req.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send({
+            error: 'id not valid'
+        });
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return res.status(404).send({ todo });
+        }
+        res.send({
+            todo
+        });
+        done();
+    }).catch((err) => res.status(400).send());
 });
 
 app.listen(port, () => {
